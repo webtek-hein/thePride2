1066d2e0424b15d38595959b2b0936829b9b481c
Yanzky <yanzkytootsky@gmail.com>
2018-06-12 06:38:26 -0700

    Chicken Joy

--- register.php
+++ register.php
@@ -9,8 +9,24 @@ $addr = $_POST['addr'];
 $num = $_POST['num'];
 $type = $_POST['type'];
 $user = $_POST['username'];
+
+$sql = "SELECT username FROM user";
+$t = $conn->query($sql);
+    while ($row = $t->fetch_assoc()) {
+          if($row['username'] == $user){
+              $m = "Username, Already Exist!";
+              echo "
+                <script type = 'text/javascript'>
+                    alert('$m');
+                    window.location.replace('index.php');
+                </script>
+             ";
+          }
+    }
+
+
 $pass = $_POST['pass'];
-if($_POST['pass'] != $_POST['pass2']){
+if ($_POST['pass'] != $_POST['pass2']) {
     $m = "Password not Match!";
     echo "
             <script type = 'text/javascript'>
@@ -18,12 +34,12 @@ if($_POST['pass'] != $_POST['pass2']){
                 window.location.replace('index.php#toregister');
             </script>
          ";
-}else{
+} else {
     $sql = "INSERT INTO user(firstname,lastname,address,contact_No,acctype,username,password,status) VALUES 
                 ('$fname','$lname','$addr','$num','$type','$user','$pass','pending')";
 
     $r = $conn->query($sql);
-    if(!$r){
+    if (!$r) {
         var_dump($conn->error);
         die;
 
